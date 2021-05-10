dfe26da20856384bb2932496f5648d367f520411
lanangwisnugiri <lanangwisnugiri@gmail.com>
2021-05-10 15:48:04 +0800

    added .gitignore file

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+// removes all unused webpack assets after every successful rebuild
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: ["./assets/scripts/main.js", "./assets/styles/main.scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new CleanWebpackPlugin()],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
