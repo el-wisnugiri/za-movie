7d6d1735f6e46a7b67e6198e43cdd553ebae7409
lanangwisnugiri <lanangwisnugiri@gmail.com>
2021-05-11 17:26:47 +0800

    fix: fixed bug and asynchronously fetch data

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,41 +1,49 @@
 // click event for search button
 document.querySelector(".search-button").addEventListener("click", function () {
-  searchMovies();
+  renderMoviesCard();
 });
 
-// search function
-function searchMovies() {
-  const apiKey = "867a6158";
+// asynchronously fetch the data everytime the user click on search
+async function getMovies() {
   let url = "http://www.omdbapi.com/";
+  const apiKey = "867a6158";
   let inputValue = document.querySelector(".search-input").value;
 
-  // TODO: make this async function
-  fetch(`${url}?apikey=${apiKey}&s=${inputValue}`)
-    .then((res) => res.json())
-    .then((movies) => {
-      movies.Search.forEach((movie) => {
-        // console.log(movie.Title);
-        let moviesWrapper = document.querySelector(".list-movies");
-        moviesWrapper.innerHTML +=
-          `<div class="col-md-4 mb-3 mt-3">
-        <div class="card" style="width: 18rem;">
-        <img src="` +
-          movie.Poster +
-          `" class="card-img-top" height="300px">
-        <div class="card-body"><h5 class="card-title">` +
-          movie.Title +
-          `</h5>
-        <p class="card-text">Year : ` +
-          movie.Year +
-          `</p>
-        <a href="#" class="btn btn-primary" id="btn-details" data-id="` +
-          movie.imdbID +
-          `"
-        data-toggle="modal" data-target="#exampleModal">Show Details</a>
-        </div></div></div>`;
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  try {
+    let res = await fetch(`${url}?apikey=${apiKey}&s=${inputValue}`);
+    //console.log(res.json());
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+// asynchrounously render cards containing data of movies by search filter
+async function renderMoviesCard() {
+  let movies = await getMovies();
+  let html = "";
+
+  movies.Search.forEach((movie) => {
+    let hmtlSegment =
+      `<div class="col-md-4 mb-3 mt-3">
+    <div class="card" style="width: 18rem;">
+    <img src="` +
+      movie.Poster +
+      `" class="card-img-top" height="300px">
+    <div class="card-body"><h5 class="card-title">` +
+      movie.Title +
+      `</h5>
+    <p class="card-text">Year : ` +
+      movie.Year +
+      `</p>
+    <a href="#" class="btn btn-primary" id="btn-details" data-id="` +
+      movie.imdbID +
+      `"
+    data-toggle="modal" data-target="#exampleModal">Show Details</a>
+    </div></div></div>`;
+
+    html += hmtlSegment;
+  });
+
+  let moviesWrapper = document.querySelector(".list-movies");
+  moviesWrapper.innerHTML = html;
 }
