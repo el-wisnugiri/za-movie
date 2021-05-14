032c62365183945cb2367b2a6a3bd33ec499acb2
LanangWisnugiri <50191634+el-wisnugiri@users.noreply.github.com>
2021-05-14 14:02:21 +0800

    added popup containing details of each cards

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -22,7 +22,6 @@ async function getMovies() {
 
   try {
     let res = await fetch(`${url}?apikey=${apiKey}&s=${inputValue}`);
-    //console.log(res.json());
     return await res.json();
   } catch (error) {
     console.log(error);
@@ -37,22 +36,16 @@ async function renderMoviesCard() {
   movies.Search.forEach((movie) => {
     let hmtlSegment =
       `<div class="col-md-4 mb-3 mt-3 d-flex justify-content-center">
-    <div class="card" style="width: 18rem;">
-    <img src="` +
-      movie.Poster +
-      `" class="card-img-top" height="300px">
-    <div class="card-body"><h5 class="card-title">` +
-      movie.Title +
-      `</h5>
-    <p class="card-text">Year : ` +
-      movie.Year +
-      `</p>
-      <div class="d-flex align-items-center justify-content-center">
-    <a href="#" class="btn btn-secondary button-details" id="btn-details" data-id="` +
-      movie.imdbID +
-      `"
-    data-toggle="modal" data-target="#exampleModal">Show Details</a>
-    </div></div></div></div>`;
+        <div class="card movie-card" style="width: 18rem;">
+          <img src="${movie.Poster}" class="card-img-top" height="300px">
+            <div class="card-body"><h5 class="card-title">${movie.Title}</h5>
+            <p class="card-text">Year : ${movie.Year}</p>
+            <div class="d-flex align-items-center justify-content-center">
+              <a href="#" class="btn btn-secondary button-details" id="${movie.imdbID}" data-id="${movie.imdbID}" data-toggle="modal" data-target="#exampleModal">Show Details</a>
+            </div>
+          </div>
+        </div>
+      </div>`;
 
     html += hmtlSegment;
   });
@@ -61,32 +54,86 @@ async function renderMoviesCard() {
   moviesWrapper.innerHTML = html;
 }
 
-async function cardId() {
-  try {
-    let buttonDetails = await document.querySelector(".button-details");
-    let movieId = buttonDetails.getAttribute("data-id");
-    console.log(movieId);
-    //return movieId
-  } catch (error) {
-    console.log(error);
+
+// callback executed when card was found
+// get all the buttons and store them in array
+function handleCards(card) {
+  var buttonArray = document.getElementsByClassName('movie-card');
+  for (i = 0; i < buttonArray.length; i++) {
+    if (document.addEventListener) {
+      buttonArray[i].addEventListener("click", myFunction);
+    }
+    else {
+      if (document.attachEvent) {
+        buttonArray[i].attachEvent("onclick", myFunction);
+      }
+    }
   }
 }
 
-// asynchronously fetch data by their id
+// set up the mutation observer and keep observing cards result
+var observer = new MutationObserver(function (mutations, me) {
+  // `mutations` is an array of mutations that occurred
+  var card = document.querySelector('.movie-card');
+  if (card) {
+    handleCards(card);
+    //me.disconnect(); // stop observing
+    return;
+  }
+});
+
+// start observing
+observer.observe(document, {
+  childList: true,
+  subtree: true
+})
+
+// pass  the id
+async function myFunction(e) {
+  // get pop-up details when card button is clicked
+  getDetails(e.target.id);
+}
+
+// asynchronously fetch data by their id and display it on modal popup
 async function getDetails(movieId) {
   let url = "http://www.omdbapi.com/";
   const apiKey = "867a6158";
   let id = movieId;
+  let html = "";
 
-  console.log(id);
 
   try {
-    let res = await fetch(`${url}?apikey=${apiKey}&i=${id}`);
-    console.log(res.json());
-    //return await res.json();
+    fetch(`${url}?apikey=${apiKey}&i=${id}`).then(response =>
+      response.json().then(data => ({
+        movieDetails: data,
+        status: response.status
+      })
+      ).then(res => {
+        // console.log(res.status, res.movieDetails)
+        let hmtlSegment =
+          `<div class="details-image col-12 mb-md-5 d-flex justify-content-center">
+            <img src="${res.movieDetails.Poster}" alt="details - image" />
+            </div >
+            <div class="movie-details col-12">
+              <ul class="list-group">
+                <li class="list-group-item">
+                  <h4 class="d-flex justify-content-center">${res.movieDetails.Title}</h4>
+                </li>
+                <li class="list-group-item"><b>Released:</b> ${res.movieDetails.Released}</li>
+                <li class="list-group-item"><b>Genre:</b> ${res.movieDetails.Genre}</li>
+                <li class="list-group-item"><b>Writer:</b> ${res.movieDetails.Writer}</li>
+                <li class="list-group-item"><b>Actors:</b> ${res.movieDetails.Actors}</li>
+                <li class="list-group-item"><b>Rated:</b> ${res.movieDetails.Rated}</li>
+                <li class="list-group-item"><b>Plot:</b> ${res.movieDetails.Plot}</li>
+              </ul>
+            </div>`;
+
+        html += hmtlSegment;
+
+        let modalWrapper = document.querySelector(".modal-body");
+        modalWrapper.innerHTML = html;
+      }));
   } catch (error) {
     console.log(error);
   }
 }
-
-// TODO: show movies details on modal with the correct IMDB ID
